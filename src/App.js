352e3574047c380e2ce76cbdf0fd5e68a58a39c3
Li <35750523+vCel@users.noreply.github.com>
2022-04-01 15:04:24 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is not the app you are looking for...</h1>
+    </div>
+  );
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// navigation links
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Book</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
