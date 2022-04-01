5a6801fecc91078f18ccfc97a7d863490c28c5c0
Li <35750523+vCel@users.noreply.github.com>
2022-04-01 15:31:25 +1000

    changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Pages
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Book from "./pages/Book";
+import About from "./pages/About";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>This is not the app you are looking for...</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>This is not the app you are looking for...</h1>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        {/* <Home/> */}
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // navigation links
 export default function Nav() {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Book</li>
-        <li>About</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/book">Book</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
